1d14a00e719562a6d33b2586dab33a77ce3b1d62
douglasvcv <douglasvitor1233@gmail.com>
2024-10-29 23:30:03 -0300

    add Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+function Navbar(){
+  return(
+    <div className="navbar_container">
+        <div className="navbar_options">
+        <a href="#">Home</a>
+        <a href="#">Categorias</a>
+        <a href="#">Fale Conosco</a>
+        <a href="#">Últimas notícias</a>
+        </div>
+        <div className="navbar_button">
+            <button>Inscreva-se</button>
+        </div>
+    </div>
+)
+}
+
+export default Navbar
